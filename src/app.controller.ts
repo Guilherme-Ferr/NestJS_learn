f21d4b5509978deb8afefb7d9832a1a94398bc2b
GUILHERME FERREIRA DA SILVA <guilherme.ferreira@grupovestcasa.local>
2022-12-09 16:56:18 -0300

    feat: classe para o body da requisição

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,13 +2,16 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { RocketTeamMember } from '@prisma/client';
 import { randomUUID } from 'crypto';
 import { PrismaService } from './database/prisma.service';
+import { CreateTeamMemberBody } from './dtos/create-team-member-dto';
 
 @Controller('users')
 export class AppController {
   constructor(private prisma: PrismaService) {}
 
   @Post('create')
-  async getHello(@Body() body: any): Promise<RocketTeamMember> {
+  async getHello(
+    @Body() body: CreateTeamMemberBody,
+  ): Promise<RocketTeamMember> {
     const { name, fun } = body;
 
     return await this.prisma.rocketTeamMember.create({
